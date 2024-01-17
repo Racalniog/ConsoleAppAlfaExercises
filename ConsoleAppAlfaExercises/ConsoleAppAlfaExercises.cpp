c1390a9ec621619105782416ca8dff2822e2e277
giancarlostooss <giancarlostooss@gmail.com>
2024-01-17 10:52:45 +0100

    fixed input handling

--- ConsoleAppAlfaExercises/ConsoleAppAlfaExercises.cpp
+++ ConsoleAppAlfaExercises/ConsoleAppAlfaExercises.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	string userInputString;
-	double userInputNumber;
+	double userInputNumber = {};
 	vector<unique_ptr<BaseFunction>> functions;
 	functions.push_back(make_unique<ReverseStr>());
 
@@ -11,33 +11,32 @@ int main()
 		cout << "Choose a function(0,1,2...) (or type 'q' to quit).'\n' ";
 		for (int i = 0; i < functions.size(); i++)
 			cout << i << ". " << functions[i]->getName() << '\n';
-		
 		cin >> userInputString;
 		if (userInputString == "q") {
 			break;
 		}
-		istringstream iss(userInputString);
-		if (iss >> userInputNumber) {
-			cout << "Entered number: " << userInputNumber << '\n';
-		}
-		else {
-			cout << "Entered string: " << userInputString << '\n';
+		while (true) {
+			istringstream iss(userInputString);
+			if (iss >> userInputNumber && userInputNumber <= functions.size()-1) {
+				cout << "Entered number: " << userInputNumber << '\n';
+				break;
+			}
+			else {
+				cout << "Please enter a number to choose function\n";
+			}
+			cin >> userInputString;
 		}
 		cout << "Enter string to manipulate: ";
 		cin >> userInputString;
 		auto startTime = chrono::high_resolution_clock::now();
 		
 		if (functions.size() >= userInputNumber) {
-			cout << '\n' << functions[userInputNumber]->execute(userInputString);
+			cout << functions[userInputNumber]->execute(userInputString) << '\n';
 		}
 
 		auto endTime = chrono::high_resolution_clock::now();
-
 		auto duration = chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
-		iss = {};
-		userInputString = {};
-		userInputNumber = {};
-		cout << " Time taken by function: " << duration << " microseconds\n";
+		cout << " Time taken by function: " << duration << " microseconds" << endl;
 	}
 	cout << "Quitting app" << endl;
 }
