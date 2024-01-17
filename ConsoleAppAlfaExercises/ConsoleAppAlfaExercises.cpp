4b699c77a624f3e7b285695342b2d0c5267f1ae8
giancarlostooss <giancarlostooss@gmail.com>
2024-01-17 10:35:43 +0100

    clean up

--- ConsoleAppAlfaExercises/ConsoleAppAlfaExercises.cpp
+++ ConsoleAppAlfaExercises/ConsoleAppAlfaExercises.cpp
@@ -1,17 +1,9 @@
 #include "Header.h"
-
 //TODO add input handler with variable input, only ask for input after function is selected
-template<typename T>
-using FuncStrPtr = function<void(const T)>;
-
-
-
 int main()
 {
 	string userInputString;
 	double userInputNumber;
-	string command;
-
 	vector<unique_ptr<BaseFunction>> functions;
 	functions.push_back(make_unique<ReverseStr>());
 
@@ -20,7 +12,6 @@ int main()
 		for (int i = 0; i < functions.size(); i++)
 			cout << i << ". " << functions[i]->getName() << '\n';
 		
-		//getline(cin, userInputString);
 		cin >> userInputString;
 		if (userInputString == "q") {
 			break;
@@ -40,12 +31,6 @@ int main()
 			cout << '\n' << functions[userInputNumber]->execute(userInputString);
 		}
 
-		/*cout << reverseString(userInputString) << '\n';
-		cout << removeSpaces(userInputString) << '\n';
-		cout << countNumsInString(userInputString) << '\n';
-		cout << removeLeadingSpaces(userInputString) << '\n';
-		cout << removeTrailingSpaces(userInputString) << '\n';*/
-
 		auto endTime = chrono::high_resolution_clock::now();
 
 		auto duration = chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
@@ -54,8 +39,6 @@ int main()
 		userInputNumber = {};
 		cout << " Time taken by function: " << duration << " microseconds\n";
 	}
-
 	cout << "Quitting app" << endl;
-
 }
 
